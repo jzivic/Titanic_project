76f8318a85baf9e6e7a9a60d1a439ffde8251141
jzivic <jzivic@fsb.hr>
2022-02-01 00:38:47 +0100

    Initial commit

---
+++ A_preprocessing/Preprocessing.py
@@ -0,0 +1,441 @@
+"""
+Prvi dio projekta: učitavaju se podaci, pravi njihov pregled i provjerava nedostaju li podaci u
+pojedinim kategorijama. Nakon toga analiziraju se vrste varijabli i raspodjele podataka te se određuje što će se
+napraviti s nepotpunim podacima. Kako bi se dobili što bolji rezultati, podaci će se skalirati na 2 načina,
+dići u prostor viših značajki te provesti Principal Component Analysis (PCA) analizu.
+"""
+
+import pickle, shutil, os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn import decomposition
+
+global project_path
+project_path = "C:/Users/Josip/PycharmProjects/Titanic_project/"
+
+os.chdir(project_path)
+# Učitavanje podataka u DataFrame
+train_df = pd.read_csv('A_preprocessing/train.csv')
+test_df = pd.read_csv('A_preprocessing/test.csv')
+all_df = train_df.append(test_df)
+
+##################################        1. Statistika i opći pregled        #########################################
+
+
+
+# Pregled varijabli i eventualni nedostatak podataka:
+# print(train_df.info())
+# print(test_df.info())
+
+
+"""
+Pregledom svih varijabli došlo se do sljedećih zaključaka:
+Kategoričke varijable: Survived, Sex, Embarked
+Ordinalne: Pclass
+Numeričke: Age, Fare
+Diskretne: SibSp, Parch
+
+Age i Cabin imaju nepotpune podatke u train i test setu: nadopunjavati ili izbaciti?
+
+Pretpostavke:
+Žene, djeca i putnici 1. klase imaju veću vjerojatnost preživljavanja
+"""
+
+
+
+# Opisna funkcija koja daje opći pregled/grafove preživljavanja po pojedinoj kateogriji
+def survived_in_category(category):
+    wanted_data = train_df[[category, 'Survived']]       # podaci željene kategorije i preživljavanja
+    number_in_category = wanted_data.groupby(category).size()     # broj ljudi podijeljen po kategoriji
+
+    survived_people = wanted_data[wanted_data["Survived"] == 1]       # preživjeli iz ukupnog popisa
+    died_people = wanted_data[wanted_data["Survived"] == 0]
+
+    # sortiran udio preživjelih u svakoj kateogoriji
+    survived_rate = wanted_data.\
+        groupby([category], as_index=False).mean()\
+        .sort_values(by='Survived', ascending=False)
+
+
+
+    # delete folder for category if exists and create an empty one
+    try:
+        shutil.rmtree("diagrams/"+category)
+    except:
+        FileNotFoundError
+    os.mkdir("diagrams/"+category)
+
+
+    # Ukupan broj ljudi po kategoriji
+    def overall_number():
+        print("Ukupan broj ljudi u kategoriji: " + category)
+        print(number_in_category)
+        fig = plt.gcf()
+        plt.title("Overall people in category:  " + category)
+        plt.ylabel("Number of people")
+        plt.xlabel("Category")
+        plt.bar(survived_rate[category], number_in_category)
+        plt.draw()
+        plt.savefig("diagrams/"+category+"/Overall people in "+ category+".png", dpi=300)
+        plt.clf()
+
+
+    # Postotak preživjelih ljudi po kategoriji
+    def survival_percentage():
+        print("\nSurvival percentage in category: " + category)
+        print(survived_rate)
+        fig = plt.gcf()
+        plt.title("Survival percentage in category : " + category)
+        plt.ylabel("Survival percentage")
+        plt.xlabel("Category")
+        plt.bar(survived_rate[category], survived_rate["Survived"]*100)
+        plt.draw()
+        plt.savefig("diagrams/"+category+"/Survival percentage in "+ category+".png", dpi=300)
+        plt.clf()
+
+
+    # Odnos brojeva preživjelih i umrlih ljudi po kategoriji (isti grafovi, drugi način)
+    def survived_vs_died():
+        fig = plt.gcf()
+        plt.title("Survived and died ratio:  " + category)
+        plt.ylabel("Number of people")
+        plt.xlabel(category)
+        plt.hist(died_people[category], bins=10, color="black", edgecolor="red",  alpha=0.5,  label="died")
+        plt.hist(survived_people[category], bins=10, color="blue", edgecolor="red", alpha=0.5, label="survived")
+        plt.legend()
+        plt.draw()
+        plt.savefig("diagrams/"+category+"/Survived and died ratio in "+ category+".png", dpi=300)
+        plt.clf()
+
+    try:
+        overall_number()
+        survival_percentage()
+        survived_vs_died()
+    except:
+        pass
+
+
+# survived_in_category("Pclass")
+# survived_in_category("Sex")
+# survived_in_category("Age")
+# survived_in_category("SibSp")
+# survived_in_category("Parch")
+# survived_in_category("Fare")
+# survived_in_category("Embarked")
+
+
+"""
+Zaključci:
+Preživljavanje je najviše vezano uz: Sex, Age, Pclass.
+Također je manje vezano uz Embarked, Fare, Parch, SibSp.
+Age i Fare treba transformirati u kategoričku varijablu  i odrediti raspone.
+
+PassangerId, Name i Ticket se izbacuju jer su slučajne varijabla i ne koreliraju s preživljavanjem.
+Cabin se izacuje jer za većinu putnika ne postoji podatak.
+Izbacuju se nepotpuni podaci jer cjelovitih podataka ima dovoljno za treniranje modela.
+"""
+
+
+
+
+
+###################################        2. Transformiranje podataka        #########################################
+
+
+# Transformacija kontinuiranih varijabli u kategoričke
+def transform_data(input_df, set_for_train):
+
+    # Age se dijeli po osobnim, realnim kategorijama, (5 starosnih kategorija)
+    input_df["Age"] = pd.cut(input_df["Age"],bins=[0,5,18,35,60,300],
+                            labels=[1,2,3,4,5])
+
+    # Fare se dijeli osvisno o distribuciji na temelju procjene i razlike iz grafa (7 kategorija)
+    input_df["Fare"] = pd.cut(input_df["Fare"],bins=[0,10,30,50,80,120,300,600],
+                             labels=[1,2,3,4,5,6,7])
+
+    # Sex i Embarked se jednostavno preslikavaju
+    input_df["Sex"] = input_df["Sex"].map({"male":0, "female":1})
+    input_df["Embarked"] = input_df["Embarked"].map({"C":1, "Q":2, "S":3})
+
+    # Izbacivanje nepotrebnih podataka
+    input_df = input_df.drop(columns=["PassengerId", "Name", "Ticket", "Cabin"])       # izbacivanje nepotrebnih kolona
+
+    # Dijeljenje train i test seta
+    if set_for_train == True:
+        input_df = input_df.dropna()                        # Izbacivanje nepotpunih podataka
+        X_data = input_df.drop(columns=["Survived"])        # Za train set treba izbaciti "Survived" kategoriju
+        Y_data = input_df["Survived"]
+
+    elif set_for_train == False:
+        X_data = input_df                   # Za test set ne postoji "Survived" kategorija
+        Y_data = None
+
+    return [X_data, Y_data]
+
+
+# Transformirani podaci gdje su kontinuirane varijable prebačene u kategoriče
+train_filtered_data = transform_data(train_df, set_for_train=True)
+X_train = train_filtered_data[0]
+Y_train = train_filtered_data[1]
+
+train_filtered_data = transform_data(test_df, set_for_train=False)
+X_test = train_filtered_data[0]
+
+
+
+
+
+
+###################################        3. Manipulacije podacima        #########################################
+
+"""
+Kako bi se dobila što veća točnost modela, podaci će se transformirati u nekoliko kateorija te će se naknadno vidjeti
+koji imaju najbolju točnost. Transformacije su Standardno i MinMax skaliranje, dizanje u viši prostor značajki za 
+različit broj razina te PCA analiza odnosno redukcija značajki. 
+"""
+
+# A: skaliranje podataka: Standardno i MinMax
+def scalling_data(type_of_scaling):
+    # U slučaju pogrešne ključne riječi skaliranja dobiva se error:
+    assert type_of_scaling == StandardScaler or type_of_scaling == MinMaxScaler, "Skaliranje mora biti Std ili MinMax"
+
+    scal_data_test = pd.DataFrame()
+    categories = [categ for categ in X_train]
+    scal_data_train = pd.DataFrame()
+    if type_of_scaling == StandardScaler:
+        scaler_model = StandardScaler()
+        scal_data_train[categories] = scaler_model.fit_transform(X_train[categories])   # skaliranje svake kategorije
+        scal_data_test[categories] = scaler_model.transform(X_test[categories])
+
+    elif type_of_scaling == MinMaxScaler:
+        scaler_model = MinMaxScaler()
+        scal_data_train[categories] = scaler_model.fit_transform(X_train[categories])
+        scal_data_test[categories] = scaler_model.transform(X_test[categories])
+
+    return [scal_data_train, scal_data_test]
+
+
+scal_data = scalling_data(StandardScaler)   # Podaci s transformacijom standardiziranog skaliranja
+scal_std_X_train = scal_data[0]
+scal_std_X_test = scal_data[1]
+
+scal_data = scalling_data(MinMaxScaler)     # Podaci s MinMax skaliranjem
+scal_MM_X_train = scal_data[0]
+scal_MM_X_test = scal_data[1]
+
+
+# B: dizanje matrice dizajna u prostor više dimenzije (kombinacija pstojećih kategorija)
+def polynomial_features_scaling(deg):
+    p_X_train = sklearn.preprocessing.PolynomialFeatures(degree=deg).fit_transform(X_train)
+    p_X_test = sklearn.preprocessing.PolynomialFeatures(degree=deg).fit_transform(X_test)
+    poly_X_train = pd.DataFrame(p_X_train)
+    poly_X_test = pd.DataFrame(p_X_test)
+
+    return [poly_X_train, poly_X_test]
+
+
+poly3_data = polynomial_features_scaling(deg=3)         # Podaci dignuti za 3 stupnja
+poly3_X_train = poly3_data[0]
+poly3_X_test = poly3_data[1]
+
+poly4_data = polynomial_features_scaling(deg=4)         # Podaci dignuti za 4 stupnja
+poly4_X_train = poly4_data[0]
+poly4_X_test = poly4_data[1]
+
+
+
+# C: Principal component analysis: redukcije značajki i utjecaj na vrijeme/točnost modela
+# Provjera točnosti podataka nakon redukcije: bitno je koristiti već skalirane podatke
+def PCA_f(n_components):
+    assert n_components <= len(X_train.columns), "Broj komponenata je prevelik!"  # max komponenata koliko može ostati
+    pca = decomposition.PCA(n_components=n_components)
+    pca_X_train = pca.fit_transform(scal_std_X_train)
+    pca_X_test = pca.fit_transform(scal_std_X_test)
+
+    # Singular value decomposition
+    U, sigma, V = np.linalg.svd(pca_X_train)  # lijeva ort. matrica , matrica vlastitih vrijednosti, desna ort. matrica
+    sum_current_sigma = sum([sigma[i] for i in range(n_components)])
+    sum_whole_sigma = sum(np.linalg.svd(decomposition.PCA(n_components=7).fit_transform(scal_std_X_train))[1])
+    data_preservation = (sum_current_sigma/sum_whole_sigma)         # očuvanost podataka nakon transformacije
+    # print("Očuvanost podataka: ", round(data_preservation,2))
+
+    return [pca_X_train, pca_X_test]
+
+
+pca_6_X = PCA_f(6)
+pca_6_X_train = pca_6_X[0] # Podaci reducirani za jednu značajku
+pca_6_X_test = pca_6_X[1]
+pca_5_X = PCA_f(5)
+pca_5_X_train = pca_5_X[0] # Podaci reducirani za dvije značajku
+pca_5_X_test = pca_5_X[1]
+pca_4_X = PCA_f(4)
+pca_4_X_train = pca_4_X[0] # Podaci reducirani za jednu značajku
+pca_4_X_test = pca_4_X[1]
+
+
+
+
+# Dodatna provjera: Ima li smisla skalirati podatke nakon dizanja u viši prostor značajki - NEMA
+def poly_and_scaling():
+    poly4_data = polynomial_features_scaling(deg=4)
+    poly4_X_train = poly4_data[0]
+    poly4_X_test = poly4_data[1]
+
+    scal_data_test = pd.DataFrame()
+    categories = [categ for categ in poly4_X_train]
+    scal_data_train = pd.DataFrame()
+    scaler_model = StandardScaler()
+
+    scal_data_train[categories] = scaler_model.fit_transform(poly4_X_train[categories])
+    scal_data_test[categories] = scaler_model.transform(poly4_X_test[categories])
+
+    train_data = scal_data[0]
+    test_data = scal_data[1]
+    return[train_data, test_data]
+
+poly_and_scaling_data = poly_and_scaling()
+poly_scal_train = poly_and_scaling_data[0]
+poly_scal_test = poly_and_scaling_data[1]
+
+
+
+# Rječnik sa svim test podacima za predviđanje
+all_X_test_data = { "X": X_test,
+                    "scal_std_X": scal_std_X_test,
+                    "scal_MM_X": scal_MM_X_test,
+                    "poly3_X": poly3_X_test,
+                    "poly4_X": poly4_X_test,
+                    "pca_6_X": pca_6_X_test,
+                    "pca_5_X": pca_5_X_test,
+                    "pca_4_X": pca_4_X_test,
+                    "poly_scal_X": poly_scal_test,
+                   }
+
+
+
+with open('A_preprocessing/all_X_test_data.pickle', 'wb') as f_test:    # spremanje riječnika u dictdf
+    pickle.dump(all_X_test_data, f_test)
+
+# Rječnik sa svim ukupnim train podacima
+all_X_train_data = {"X": X_train,
+                    "scal_std_X": scal_std_X_train,
+                    # "scal_MM_X": scal_MM_X_train,
+                    # "poly3_X": poly3_X_train,
+                    # "poly4_X": poly4_X_train,
+                    # "pca_6_X": pca_6_X_train,
+                    # "pca_5_X": pca_5_X_train,
+                    # "pca_4_X": pca_4_X_train,
+                    # "poly_scal_X": poly_scal_train
+                    }
+
+
+# Skup za treniranje se dijeli na skup za treniranje i skup za validaciju modela
+X_train_data, X_valid_data, Y_train_data, Y_valid_data, = {}, {}, {}, {}
+for data in all_X_train_data:
+    X_train, X_valid, y_train, y_valid = train_test_split(all_X_train_data[data], Y_train, random_state=11, test_size=0.1)
+    # X_train, X_valid, y_train, y_valid = train_test_split(all_X_train_data[data], Y_train, test_size=0.1)
+    X_train_data[data] = X_train
+    X_valid_data[data] = X_valid
+    Y_train_data[data] = y_train    # y je ovdje, Y je sve skupa gore
+    Y_valid_data[data] = y_valid
+
+
+# Ukupni podijeljenji podaci
+divided_train_data = {
+                    "X_train_data": X_train_data,
+                    "X_valid_data": X_valid_data,
+                    "Y_train_data": Y_train_data,
+                    "Y_valid_data": Y_valid_data,
+                    }
+
+# Spremanje podataka u rječnik
+with open('A_preprocessing/divided_train_data.pickle', 'wb') as div:
+    pickle.dump(divided_train_data, div)
+
+
+
+
+
+# random_state treba provjeriti!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print()
+# DOBRE FORE
+
+# train_df['Pclass'].value_counts(sort=False).plot(kind='bar')
+# plt.show()
+
+# t_f_null = train_df["Age"].isnull()#.values.any()
+# index_null = [i for i in range(len(t_f_null)) if t_f_null[i] == True]
+
+# stariji_50 = train_df[train_df["Age"] > 50]
+# stariji_50 = train_df[train_df.Age > 50]      # Isti način zapisa
+
+
+# mapiranje / preslikavanje
+# maping = {"male":1, "female":2}
+# a = train_df["Sex"].map(maping)
+
+
+
+
+# tocnost = sklearn.metrics.accuracy_score(y_test, h)
+
+# varijablino ime se ispisuje
+# data_name = f'{variable=}'.split('=')[0]
+
+
+
+"""
+Podaci se mogu i kraće ovako transformirati ali mijenja se zapis u array umjesto DF. Koristiti način u funkciji gore
+# Podaci sa standardiziranom transformacijom
+scaler_model = StandardScaler()
+scal_std_X_train = scaler_model.fit_transform(X_train)
+scal_std_X_test = scaler_model.transform(X_test)
+# Podaci s MinMax transformacijom
+scaler_model = MinMaxScaler()
+scal_MM_X_train = scaler_model.fit_transform(X_train)
+scal_MM_X_test = scaler_model.transform(X_test)
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ B_Logistic_Regression/Logistic_regression.py
@@ -0,0 +1,199 @@
+"""
+Drugi dio: Logistička Regresija.
+Prvo je potrebno vidjeti kako se kreće točnost modela na
+različitim podacima te pronaći one koji pokazuju najbolju točnost modela. Paralelno s time potrebno je za
+svake podatke naći najbolje odgovarajući hiperparametar C=1/lambda.
+C - koeficijent dopuštene pogreške klasifikacije - obrnuto proporcionalni jačini regularizacije lambda
+Potrebno je ispitati kombinacije različith C i setova podataka
+"""
+
+from A_preprocessing.Preprocessing import project_path
+# Ukoliko se žele stvarati novi podaci svaku skriptu, ovo NE treba biti zakomentirano
+# from A_preprocessing.Preprocessing import Y_train
+# from A_preprocessing.Preprocessing import divided_train_data, all_X_test_data
+
+
+import openpyxl, pickle, math, os
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+
+# Učitavanje spremljenih podataka
+with open(project_path+'/A_preprocessing/divided_train_data.pickle', 'rb') as f_X_train:
+    divided_train_data = pickle.load(f_X_train)
+with open(project_path+'/A_preprocessing/all_X_test_data.pickle', 'rb') as f_test:
+    all_X_test_data = pickle.load(f_test)
+
+
+
+# Funkcija koja računa točnost modela za određene podatkei  hiperparametar C
+def logreg_f(C, X_train, Y_train, X_valid, Y_valid):
+    logreg_model = LogisticRegression(C=C, max_iter=1e7)        # Broj iteracija povećan zbog poly podataka
+    logreg_model.fit(X_train, Y_train)
+    acc_train = round(logreg_model.score(X_train, Y_train) * 100, 2)
+    prediction = logreg_model.predict(X_valid)
+    acc_valid = round(accuracy_score(prediction, Y_valid) *100,2)
+    # error = round(sklearn.metrics.mean_squared_error(Y_valid, prediction), 2)  # kvadratna razlika 2 vektora
+
+    return [acc_train, acc_valid]
+
+
+C_range = [10**i for i in range(-10,5)]      # raspon C hiperparametara
+xlsx_file = "B_Logistic_Regression/Acc_LogReg.xlsx"                # ime excel file u koji se spremaju rezultati, kasnije
+
+accuracy_dict = {}                           # spremanje parova (C:točnost za svaki C) za cijeli set podataka
+best_grid_acc = {}                           # spremanje najbolje točnosti za podatke
+
+
+# Iteriranje spremljenih podataka i analiza
+for data_name in divided_train_data["X_train_data"]:
+    X_train = divided_train_data["X_train_data"][data_name]
+    Y_train = divided_train_data["Y_train_data"][data_name]
+    X_valid = divided_train_data["X_valid_data"][data_name]
+    Y_valid = divided_train_data["Y_valid_data"][data_name]
+
+    max_acc = 0
+    best_grid_acc[data_name], acc_for_data = [], []
+
+    for C in C_range:
+        accurities = logreg_f(C, X_train, Y_train, X_valid, Y_valid)
+        acc_for_data.append(accurities)
+
+        if accurities[1] > max_acc:                   # spremanje nove maksimalne točnosti validacijskog seta
+            n_C = C_range.index(C)
+            max_acc = accurities[1]
+            best_grid_acc[data_name] = [n_C]
+        elif accurities[1] == max_acc:                # ako točnost već postoji, za nju se dodaju pripadajući hiperparametar C
+            n_C = C_range.index(C)
+            best_grid_acc[data_name].append(n_C)
+
+    accuracy_dict[data_name] = acc_for_data         # dodavanje svih točnosti u rječnik
+
+accuracy_DF = pd.DataFrame(accuracy_dict, index=[C_range])  # kreiranje DataFrame-a
+
+
+# Funkcija koja zapisuje sve točnosti u excel
+def to_excel():
+    accuracy_DF.to_excel(xlsx_file, sheet_name="Accuracity")                     # zapisivanje excel file-a
+    # Zapisivanje oznake za C u tablicu i bojanje oznake
+    workbook = openpyxl.load_workbook(xlsx_file)
+    workbook.get_sheet_by_name("Accuracity").cell(row=1, column=1).value = "C"
+    workbook.get_sheet_by_name("Accuracity").cell(row=1, column=1).fill = \
+        openpyxl.styles.PatternFill("solid", fgColor="00FFFF00")  # bojanje oznaka gamma, C
+
+    # Bojanje ćelija u excelu tamo gdje je najveća točnost modela za pojedine podatke
+    for data_name in accuracy_DF:
+        n_col = accuracy_DF.columns.get_loc(data_name)          # idex stupca
+
+        for n_row in (best_grid_acc[data_name]):                # idex retka
+            workbook.get_sheet_by_name("Accuracity").cell(row=n_row+2, column=n_col+2).fill = \
+                openpyxl.styles.PatternFill("solid", fgColor="00FF0000")
+    workbook.save(xlsx_file)
+
+to_excel()
+
+
+
+"""
+Funkcija plota točnost setova za treniranje i validaciju, za standardne podatke i poly4 podatke.
+Svi skalirani i PCA podaci imaju istu tendenciju rasta kao i standardni dok poly3 i poly 4 drugačije te se zato
+oni posebno plotaju.
+"""
+
+def plot_accuracity(data_sets):
+    C_log = [math.log(i[0], 10) for i in accuracy_DF.index] # logaritam(C) s bazom 10
+    for data in data_sets:
+
+        valid_acc = list(zip(*accuracy_DF[data]))[1]
+        train_acc = list(zip(*accuracy_DF[data]))[0]
+
+        color = next(plt.gca()._get_lines.prop_cycler)['color']
+        plt.plot(C_log, valid_acc,linestyle='-', c=color, label=data)
+        plt.plot(C_log, train_acc, linestyle=':',  c=color)
+        plt.grid(color='k', linestyle=':', linewidth=0.5)
+
+        fig = plt.gcf()
+        plt.xlabel("log(C) [-]")
+        plt.ylabel("Accuracity [%]")
+        plt.title("Validation and Train acc comparison")
+        plt.draw()
+
+    plt.legend()
+    # plt.show()
+    fig.savefig("B_Logistic_Regression/plot_LogReg.png", dpi=300)
+
+
+# plot_accuracity(data_sets= ["X", "poly4_X"])
+plot_accuracity(data_sets= ["X", "scal_std_X"])
+
+
+
+
+def Log_Reg_coeffs():
+    X_train = divided_train_data["X_train_data"]["X"]
+    Y_train = divided_train_data["Y_train_data"]["X"]
+    X_valid = divided_train_data["X_valid_data"]["X"]
+    Y_valid = divided_train_data["Y_valid_data"]["X"]
+
+    logreg_model = LogisticRegression(C=C, max_iter=1e7)        # Broj iteracija povećan zbog poly podataka
+    logreg_model.fit(X_train, Y_train)
+    acc_train = round(logreg_model.score(X_train, Y_train) * 100, 2)
+    prediction = logreg_model.predict(X_valid)
+    acc_valid = round(accuracy_score(prediction, Y_valid) *100,2)
+
+    coeff_df = pd.DataFrame(X_train.columns)
+    coeff_df.columns = ['Feature']
+    coeff_df["Correlation"] = pd.Series(logreg_model.coef_[0])
+    coeff_df = coeff_df.sort_values(by='Correlation', key=abs, ascending=False)
+    coeff_df = coeff_df.reset_index(drop=True)
+    print(coeff_df)
+
+# Log_Reg_coeffs()
+
+
+
+
+
+
+"""
+Zaključci:
+    - Nakon ispitivanja zaključak je da su za različite podatke različiti hiperparametri C najbolji izbor,
+        ovisno o podacima.
+
+    - Točnost modela jako ovisi o odabiru random_state-a i podacima koji se uzimaju za train i validation set!
+
+    - Najbolju točnost train seta pokazuju podaci koji su preslikani u višu dimenziju i to za 4 razine: točnost 88.23%
+        -> logično jer je model s više parametara lakše postaje prenaučen. 
+
+    - Skaliranje podataka utječe na model logističke regresije. Za ovaj primjer negativno jer unosi oscilacije u 
+        točnost modela od slučaja od slučaja dijeljenja istog seta podataka i nerijetko smanjuje točnost.
+        -> treba imati na umu da su ovdje svi podaci prebačeni u kategoričke varijable
+        
+            
+    - Nakon provedene PCA analize dobiva se nešto manja točnost modela koja se manje smanjuje sa smanjivanjem
+        broja dimenzija. Međutim konvergencija je značajno brža: koristan alat za veće skupove podataka 
+   
+    -Nema smisla dizati podatke u viši prostor značjki i potom ih skalirati 
+    
+    - Za pravi set podataka može se očekivati malo manja točnost jer na dosta podataka fali kategorija: Age
+       
+    - Odabrani podaci: prvotni X set podaci i C=100 
+    
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
